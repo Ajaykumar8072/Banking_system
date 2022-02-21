92c0bc9ccd511e9ed0e3a4c77108ebae5283a105
Ajaykumar8072 <100142536+Ajaykumar8072@users.noreply.github.com>
2022-02-21 19:45:14 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+	$servername = "localhost";
+	$username = "root";
+	$password = '';
+	$dbname = "zombie bank"; 
+
+	$conn = mysqli_connect($servername, $username, $password, $dbname) or dir("unable to connect");
+
+
+?>
